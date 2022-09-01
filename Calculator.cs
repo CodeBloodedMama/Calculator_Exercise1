43c5761691dc43795662b82cf974ef4b21f9ca1c
Marcin Szymanek <48028985+MarcinSzymanek@users.noreply.github.com>
2022-09-01 11:43:58 +0200

    Added Calculator class and Test

---
+++ Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return MathF.Round((float)a * (float)b);
+        }
+
+        public double Power(double a, double b)
+        {
+            return MathF.Round(MathF.Pow((float)a, (float)b));
+        }
+    }
+}
