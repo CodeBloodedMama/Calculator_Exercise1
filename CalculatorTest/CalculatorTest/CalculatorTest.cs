a5604ff22655d1a5fb17b6518ed5b1dd2f21dbbe
au618503 <58029607+au618503@users.noreply.github.com>
2022-09-14 22:19:34 +0200

    Added test: AddTest(), TestAccumulator(). And changed TestSubstract()
    TestSubstract() allows a difference of 0.001

--- CalculatorTest/CalculatorTest/CalculatorTest.cs
+++ CalculatorTest/CalculatorTest/CalculatorTest.cs
@@ -6,7 +6,9 @@ namespace CalculatorApp
     public class CalculatorTest
     {
         private Calculator calc1;
-        private Calculator calc2;
+
+        //private Calculator calc2;
+        //private Calculator calc3;
 
         [SetUp]
         protected void SetUp()
@@ -17,9 +19,15 @@ namespace CalculatorApp
         [Test]
         public void AddTest()
         {
+            // Test 1
             double a = 2.3;
             double b = 3.2;
             Assert.AreEqual(5.5, calc1.Add(a, b));
+           
+            // Test 2
+            a = -4;
+            b = -6.5;
+            Assert.AreEqual(-10.5, calc1.Add(a, b));
         }
 
         [Test]
@@ -28,6 +36,7 @@ namespace CalculatorApp
             int a = 2;
             int b = 3;
             Assert.AreEqual(5, calc1.Add(a, b));
+
         }
 
         [Test]
@@ -35,11 +44,11 @@ namespace CalculatorApp
         {
             double a = 2.3;
             double b = 3.2;
-            Assert.AreEqual(-0.9, calc1.Substract(a, b));
+            Assert.AreEqual(-0.9, calc1.Substract(a, b), 0.001);
 
             a = 62;
             b = 20;
-            Assert.AreEqual(42, calc1.Substract(a, b));
+            Assert.AreEqual(42, calc1.Substract(a, b), 0.001);
         }
 
         [Test]
@@ -56,6 +65,23 @@ namespace CalculatorApp
             double a = 2;
             double b = 3;
             Assert.AreEqual(8, calc1.Power(a, b));
+        }
+
+        [Test]
+        public void TestAccumulator()
+        {
+            //get
+            double a = calc1.Accumulator;
+            Assert.AreEqual(0,a);
+
+            // set
+            calc1.Accumulator = 7;
+            calc1.Accumulator = 7;
+            Assert.AreEqual(14,calc1.Accumulator);
+
+            // clear
+            calc1.Clear();
+            Assert.AreEqual(0,calc1.Accumulator);
         }
     }
 }
