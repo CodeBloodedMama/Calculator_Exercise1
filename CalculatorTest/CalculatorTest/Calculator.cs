c1321f207cc6528df438aaf22564f243d53e7f2d
au618503 <58029607+au618503@users.noreply.github.com>
2022-09-14 22:14:08 +0200

    Tilføjelse af funktioner
    Fra Lab Exercise: Git basics with the Calculator

--- CalculatorTest/CalculatorTest/Calculator.cs
+++ CalculatorTest/CalculatorTest/Calculator.cs
@@ -1,27 +1,81 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
 namespace CalculatorApp
 {
     public class Calculator
     {
+        private double accumulator;
         public double Add(double a, double b)
         {
             return a + b;
+        }
+        public double Add(double addend)
+        {
+            return addend + accumulator;
         }
 
         public double Substract(double a, double b)
         {
             return a - b;
         }
+        public double Substract(double subtractor)
+        {
+            return accumulator - subtractor;
+        }
 
         public double Multiply(double a, double b)
         {
-            return MathF.Round((float)a * (float)b);
+            return a * b;
+        }
+        public double Multiply(double multiplier)
+        {
+            return multiplier * accumulator;
         }
 
-        public double Power(double a, double b)
+        public double Power(double x, double exp)
         {
-            return MathF.Round(MathF.Pow((float)a, (float)b));
+            return Math.Pow(x, exp);
         }
+        public double Power(double exponent)
+        {
+            return Math.Pow(accumulator, exponent);
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                // throw exception
+                throw new DivideByZeroException("Can't divide by 0");
+            }
+            else
+            {
+                return dividend / divisor;
+            }
+        }
+        public double Divide(double divisor)
+        {
+            if (divisor == 0)
+            {
+                // throw exception
+                throw new DivideByZeroException("Can't divide by 0");
+            }
+            else
+            {
+                return accumulator / divisor;
+            }
+        }
+
+        public double Accumulator
+        {
+            get { return accumulator; }
+            set { accumulator += value; }
+        }
+        public void Clear()
+        { accumulator = 0; }
+
+
     }
 }
