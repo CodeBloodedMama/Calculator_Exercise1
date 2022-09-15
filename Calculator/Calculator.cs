01bd9bb474b145c7b13ac98e49af18282262a8da
Marcin Szymanek <202009418@post.au.dk>
2022-09-15 10:54:14 +0200

    Deleted duplicate folders
    Deleted duplicate folders.
    Added project reference

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -1,31 +1,87 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Transactions;
 
 namespace CalculatorApp
 {
     public class Calculator
     {
+        private double accumulator;
         public double Add(double a, double b)
         {
-            decimal result = (decimal)(a + b);
-            return (double)result;
+            return a + b;
+        }
+        public double Add(double added)
+        {
+            accumulator = accumulator + added;
+            return accumulator;
         }
 
         public double Substract(double a, double b)
         {
-            double result = a - b;
-            return result;
+            return a - b;
+        }
+        public double Substract(double substractor)
+        {
+            accumulator = accumulator - substractor;
+            return accumulator;
         }
 
         public double Multiply(double a, double b)
         {
-            decimal result = (decimal)(a * b);
-            return (double)result;
+            return a * b;
+        }
+        public double Multiply(double multiplier)
+        {
+            accumulator = multiplier * accumulator;
+            return accumulator;
         }
 
-        public double Power(double a, double b)
+        public double Power(double x, double exp)
         {
-            decimal result = (decimal)MathF.Pow((float)a, (float)b);
-            return (double)result;
+            return Math.Pow(x, exp);
         }
+        public double Power(double exponent)
+        {
+            accumulator = Math.Pow(accumulator, exponent);
+            return accumulator;
+        }
+
+        public double Divide(double dividend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                // throw exception
+                throw new DivideByZeroException("Can't divide by 0");
+            }
+            else
+            {
+                return dividend / divisor;
+            }
+        }
+        public double Divide(double divisor)
+        {
+            if (divisor == 0)
+            {
+                // throw exception
+                throw new DivideByZeroException("Can't divide by 0");
+            }
+            else
+            {
+                accumulator = accumulator / divisor;
+                return accumulator;
+            }
+        }
+
+        public double Accumulator
+        {
+            get { return accumulator; }
+            set { accumulator += value; }
+
+        }
+        public void Clear()
+        { accumulator = 0; }
+
+
     }
 }
